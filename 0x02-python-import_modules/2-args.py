21b607814ff7885ec338f5f39547881e1e99e1db
Castro Hamiltone <castromiltone@gmail.com>
2023-05-11 11:28:28 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    arg_count = len(sys.argv)
+    if arg_count >= 3:
+        print("{} arguments:".format(arg_count))
+        for i in range(1, arg_count):
+            print("{}: {}".format(i, sys.argv[i]))
+    elif arg_count == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("0 arguments.")
