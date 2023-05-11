4dab8cfcc23c48bed5779a65d26082409d9d148f
Castro Hamiltone <castromiltone@gmail.com>
2023-05-11 10:39:42 -0700

    1-calculator fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-calc = __import__('calculator_1')
-a = 10
-b = 5
-print('{} + {} = {}'.format(a, b, calc.add(a, b)))
-print('{} - {} = {}'.format(a, b, calc.sub(a, b)))
-print('{} * {} = {}'.format(a, b, calc.mul(a, b)))
-print('{} / {} = {}'.format(a, b, calc.div(a, b)))
-
 if __name__ == "__main__":
-    sys.exit(0)
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
