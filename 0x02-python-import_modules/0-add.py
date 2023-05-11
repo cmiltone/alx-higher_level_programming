cb99c25d9fe2997e5b60af8fd5eae94c1adbafc8
Castro Hamiltone <castromiltone@gmail.com>
2023-05-11 10:08:42 -0700

    0-add.py - fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import sys
-def func():
+if __name__ == "__main__":
     add = __import__('add_0').add
     a = 1
     b = 2
     print('{} + {} = {}'.format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    func()
     sys.exit(0)
