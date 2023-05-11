d5213901282926431538711f8c72a1f4a8d5103b
Castro Hamiltone <castromiltone@gmail.com>
2023-05-11 09:42:51 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-func = __import__('add_0').add
+add = __import__('add_0').add
 a = 1
 b = 2
-print('{} + {} = {}'.format(a, b, func(a, b)))
+print('{} + {} = {}'.format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     sys.exit(0)
