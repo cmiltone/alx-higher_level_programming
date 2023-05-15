1780345606b73c673b5e575b20e83f25c3cdb7e9
Castro Hamiltone <castromiltone@gmail.com>
2023-05-15 11:10:51 -0700

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = []
+    new_string = ""
     for c in my_string:
         if c != "c" and c != "C":
-            new_string.append(c)
-    new_string = "".join(new_string)
+            new_string = new_string + c
     return new_string
