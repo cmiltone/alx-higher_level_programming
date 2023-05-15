796e0d7dd43bd7d8520094b82d20272fab6fa875
Castro Hamiltone <castromiltone@gmail.com>
2023-05-15 11:53:09 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print("".format())
+    for i in range(0, len(matrix)):
+        row = matrix[i]
+        for j in range(0, len(row)):
+            col = row[j]
+            if j < len(row) - 1:
+                print("{}".format(col), end=" ")
+            else:
+                print("{}".format(col))
