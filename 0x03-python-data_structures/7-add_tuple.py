b47f5f1474d1a06e41ff1a380057419bae04b2de
Castro Hamiltone <castromiltone@gmail.com>
2023-05-15 12:13:07 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum_0 = 0
+    sum_1 = 0
+    if len(tuple_a) > 0 and tuple_a[0]:
+        sum_0 += tuple_a[0]
+    if len(tuple_b) > 0 and tuple_b[0]:
+        sum_0 += tuple_b[0]
+
+    if len(tuple_a) > 1 and tuple_a[1]:
+        sum_1 += tuple_a[1]
+    if len(tuple_b) > 1 and tuple_b[1]:
+        sum_1 += tuple_b[1]
+    return (sum_0, sum_1)
