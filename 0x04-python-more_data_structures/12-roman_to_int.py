52b0e989e6e7699d83b628f2313d700ef6e21f7a
Castro Hamiltone <castromiltone@gmail.com>
2023-05-17 22:35:29 +0300

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,21 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    r = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000,'IV': 4,'IX': 9,'XL': 40,'XC': 90,'CD': 400,'CM': 900}
+    r = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900
+    }
     i = 0
     num = 0
     while i < len(roman_string):
