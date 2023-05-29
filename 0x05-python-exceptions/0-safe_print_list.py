6f4d1ea370a65da32964c0fb04f749df1c054820
Castro Hamiltone <castromiltone@gmail.com>
2023-05-29 18:42:18 +0300

    codestyle fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     try:
         while n < x:
-            print("{}".format(my_list[n]), end = "")
+            print("{}".format(my_list[n]), end="")
             n += 1
     except IndexError:
-        print("".format(), end = "")
+        print("".format(), end="")
     finally:
         print("".format())
     return n
