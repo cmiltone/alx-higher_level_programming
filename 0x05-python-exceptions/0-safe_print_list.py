f4396407c49549625a317571dffe9a9c1a457013
Castro Hamiltone <castromiltone@gmail.com>
2023-05-29 18:39:40 +0300

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    try:
+        while n < x:
+            print("{}".format(my_list[n]), end = "")
+            n += 1
+    except IndexError:
+        print("".format(), end = "")
+    finally:
+        print("".format())
+    return n
