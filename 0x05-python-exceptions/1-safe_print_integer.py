4195c8efb6011a00b91b17390f6fe1441c7635b2
Castro Hamiltone <castromiltone@gmail.com>
2023-05-29 19:05:22 +0300

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="\n")
         return True
     except ValueError:
-        #print("".format(), end="")
+        # print("".format(), end="")
         return False
