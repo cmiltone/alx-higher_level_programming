06da36d57007d6bc940a80661402d8175d47100e
Castro Hamiltone <castromiltone@gmail.com>
2023-05-30 21:43:49 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a simple square module"""
+
+
+class Square:
+    """A Square Class."""
+    __size = 10
+    """Set the class size attribute"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size * self.__size
