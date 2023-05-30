64555eddb047a719d327b2934d68260da66d7b16
Castro Hamiltone <castromiltone@gmail.com>
2023-05-30 21:54:05 +0300

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This is a simple square module"""
+
+
+class Square:
+    """A Square Class."""
+    __size = 10
+    """Set the class size attribute"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter for size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size * self.__size
