a4978f86683a593da8b4787e6ae5c3d9cfcaaaf6
Castro Hamiltone <castromiltone@gmail.com>
2023-05-30 21:28:44 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a simple square module"""
+class Square:
+    """A Square Class."""
+    __size = 10
+    """Set the class size attribute"""
+    def __init__(self, size):
+        self.__size = size
