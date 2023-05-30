b3be8c56666e63023abdd12514a72292690ead27
Castro Hamiltone <castromiltone@gmail.com>
2023-05-31 00:04:12 +0300

    100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+class Node:
+    """Singly linked list node"""
+
+
+    def __init__(self, data, next_node=None):
+        """initialize Node"""
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """data getter"""
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        """data setter"""
+        if not isinstance(size, int):
+            raise TypeError("data must be an integer")
+        self.__data = data
+
+    @property
+    def next_node(self):
+        """next_node getter"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, next_node):
+        """next node setter"""
+        if next_node != None and not isinstance(next_node, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = next_node
+
+
+class SinglyLinkedList:
+    """Singly linked list"""
+
+
+    __head = None
+
+    def __init__(self):
+        """initializer"""
+        pass
+
+    def sorted_insert(self, value):
+        """inserts node"""
+        if self.__head == None:
+            self.__head = Node(value)
+        else:
+            list_ = self.__head
+
+            while list_ != None:
+                if list_.data != None:
+                    if list_.data < value:
+                        break
+                list_ = list_.next_node
+                #print("loo {}".format(list_.data))
+
+            node = Node(None)
+
+            node.next_node = list_.next_node
+            list_.next_node = node
+            self.__head = list_
