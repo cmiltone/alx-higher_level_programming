3d0fc08d351694f6f01725fdf990ac7aba66b0b5
Castro Hamiltone <castromiltone@gmail.com>
2023-06-12 21:35:04 +0300

    1-my_list.py, tests/1-my_list.txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module to inherit from list"""
+
+
+class MyList(list):
+    """ inherits from list"""
+    __list__ = []
+    def print_sorted(self):
+        """ prints sorted list items"""
+        ints = self.copy()
+        ints.sort()
+        print(ints)
+
