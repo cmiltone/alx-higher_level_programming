0349fced92fd86dd403e864199ea6d9df96ee035
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 00:00:55 +0300

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module for square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size__ = size
+    
+    def area(self):
+        """returns square area"""
+        return self.__size__ * self.__size__
+
+    def __str__(self):
+        """returns printable string for rectangle"""
+        return "[Rectangle] {}/{}".format(self.__size__, self.__size__)
