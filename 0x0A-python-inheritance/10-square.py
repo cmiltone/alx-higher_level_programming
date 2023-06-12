40d0fc4726f40102097182220297d64a759cf01e
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 00:01:35 +0300

    codestyle fixes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size__ = size
-    
+
     def area(self):
         """returns square area"""
         return self.__size__ * self.__size__
