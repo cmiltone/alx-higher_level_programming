ede5677a375cbc266ea0b5b2e99f58bf23082997
Castro Hamiltone <castromiltone@gmail.com>
2023-06-12 22:28:02 +0300

    4-inherits_from.py - codestly fixes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" module to check if the object is an instance of 
-a class that inherited (directly or indirectly) 
+""" module to check if the object is an instance of
+a class that inherited (directly or indirectly)
 from the specified class
 """
 
 
 def inherits_from(obj, a_class):
     """
-    returns True if the object is an instance of 
-    a class that inherited (directly or indirectly) 
+    returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
     from the specified class or False otherwise
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return  True
+        return True
     return False
