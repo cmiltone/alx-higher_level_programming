d9b24743d95f687340fb0140365f84e005f0345b
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 23:43:45 +0300

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module that prints pascal's triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(1, n + 1):
+        k = 1
+        t = []
+        for j in range(1, i + 1):
+            t.append(k)
+            k = k * (i - j) // j binomial coefficient
+        triangle.append(t)
+    return triangle
