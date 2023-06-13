49512db9e5095e6c992d414a91a7f0bfe0b38c19
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 21:43:06 +0300

    7-add_item.py - codestyle fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,16 @@ import sys
 
 
 def add_item():
-  """adds all arguments to list and save to file"""
-  save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-  load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    """adds all arguments to list and save to file"""
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-  filename = "add_item.json"
-  args = sys.argv[1:]
-  save_to_json_file(args, filename)
+    filename = "add_item.json"
+    args = sys.argv[1:]
+    save_to_json_file(args, filename)
 
-  data = load_from_json_file(filename)
-  print(data)
+    data = load_from_json_file(filename)
+    print(data)
 
 if __name__ == "__main__":
   add_item()
