29898d233efbae506055323e302ae86e293f1e74
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 19:51:53 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""module to read and print contents of file"""
+
+
+def read_file(filename=""):
+    """reads file and prints the content"""
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+        print(data)
+    f.close()
