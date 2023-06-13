df3fc172fa84ee2664f8fe7b501fb0ab7ece148f
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 22:07:54 +0300

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,17 @@ def add_item():
     load_from_file = __import__('6-load_from_json_file').load_from_json_file
 
     filename = "add_item.json"
+    data = []
+    try:
+        data = load_from_file(filename)
+    except (FileNotFoundError):
+        pass
     args = sys.argv[1:]
-    save_to_json_file(args, filename)
+    if len(data) > 0:
+        data = data + args
+    else:
+        data = args
+    save_to_json_file(data, filename)
 
     data = load_from_file(filename)
     print(data)
