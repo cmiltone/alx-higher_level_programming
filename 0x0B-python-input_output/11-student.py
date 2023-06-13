f5daa04250b13ff49a156fc6b3f726e5ec9baa27
Castro Hamiltone <castromiltone@gmail.com>
2023-06-13 23:02:37 +0300

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module declares a student class"""
+
+
+class Student:
+    """a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """instatiates student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """gets dictionary of instance"""
+        if attrs is None:
+            return self.__dict__
+        my_dict = {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return my_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes"""
+        self.__dict__ = json
