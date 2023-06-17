a397a6cd3c3d901c717d9412edc00276a2087b4f
Castro Hamiltone <castromiltone@gmail.com>
2023-06-17 20:12:37 +0300

    sqaure size checks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""(self, width, height, x=0, y=0, id=None)
+"""
 module for square class
 """
 from models.rectangle import Rectangle
@@ -17,5 +17,15 @@ class Square(Rectangle):
         x = self.x
         y = self.y
         w = self.width
-        h = self.height
-        return "[Square] ({}) {}/{} - {}/{}".format(id, x, y, w, h)
+        return "[Square] ({}) {}/{} - {}".format(id, x, y, w)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -12,5 +12,25 @@ class TestSqaureClass(unittest.TestCase):
 
     def test_for_correct_assignment(self):
         """Test for correct assignment"""
-        b = Square(10, 2, 1, 2)
-        self.assertEqual(2, b.id)
+        s = Square(10, 2, 1, 2)
+        self.assertEqual(2, s.id)
+
+    def test_for_correct_size_getter_setter(self):
+        """Test for correct size getter/setter"""
+        s = Square(10, 3, 1, 3)
+        s.size = 9
+        self.assertEqual(9, s.size)
+
+    def test_for_invalid_size_value(self):
+        """Test for validity of size value check"""
+        with self.assertRaises(Exception) as context:
+            s = Square(1, 2, 4)
+            s.size = -1
+        self.assertTrue("width must be >= 0" == str(context.exception))
+
+    def test_for_invalid_size_type(self):
+        """Test for validity of size type check"""
+        with self.assertRaises(Exception) as context:
+            s = Square(10)
+            s.size = "10"
+        self.assertTrue("width must be an integer" == str(context.exception))
