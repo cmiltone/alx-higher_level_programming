54ae89bbe9c0b21af11ed98ec133c8c5b87de77c
Castro Hamiltone <castromiltone@gmail.com>
2023-07-19 00:52:27 +0300

    fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- script inserts into 'first_table' table description for a database passed via command line
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best Schools');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
