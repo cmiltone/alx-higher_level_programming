18144fb3bbbedd4f1cfa60ce0967f283c5967dbd
Castro Hamiltone <castromiltone@gmail.com>
2023-07-18 23:31:30 +0300

    8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- script shows number of records in 'first_table' table ins a database passed via command line
-SELECT COUNT(`id`) from `first_table` WHERE id = 89;
+-- script shows number of records in 'first_table' table with id 89 in a database passed via command line
+SELECT COUNT(`id`) from `first_table` WHERE `id` = 89;
