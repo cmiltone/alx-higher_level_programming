4a0e7346c405a38245d139b0a9f1a3dfc22e6420
Castro Hamiltone <castromiltone@gmail.com>
2023-07-18 23:55:15 +0300

    fix

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- script shows number of records in 'first_table' table with id 89 in a database passed via command line
-SELECT COUNT(*) from `first_table` WHERE `id` = 89;
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89;
