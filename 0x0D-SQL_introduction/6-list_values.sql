2b3415adb260f3eb124f9271e9e0ff4854697c0e
Castro Hamiltone <castromiltone@gmail.com>
2023-07-18 23:17:06 +0300

    7-insert_value.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- script shows full of 'first_table' table description for a database passed via command line
+-- script shows all rows of 'first_table' table description for a database passed via command line
 SELECT * FROM first_table;
