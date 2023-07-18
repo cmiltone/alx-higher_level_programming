fd5bebe7e15d34bf54e020c3a5acc6d05973a407
Castro Hamiltone <castromiltone@gmail.com>
2023-07-19 01:31:19 +0300

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- script displays top 3 cities temp in JUly and Aug
+SELECT DISTINCT `city`, AVG(`value`) as `avg_temp` FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
