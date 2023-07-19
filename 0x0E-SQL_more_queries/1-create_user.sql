00b6b94a7cb5370fe048fc19608d16b331bc494b
Castro Hamiltone <castromiltone@gmail.com>
2023-07-19 22:52:56 +0300

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- script creates user `user_0d_1`
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`;
-ALTER USER `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON * . * TO `user_0d_1`@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
 FLUSH PRIVILEGES;
