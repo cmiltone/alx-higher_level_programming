4af58122d7803aa6ffb85224831a64eb54755007
Castro Hamiltone <castromiltone@gmail.com>
2023-07-20 01:32:19 +0300

    5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
--- script creates the table force_name
+-- script creates the table unique_id
 DROP TABLE IF EXISTS force_name;
 CREATE TABLE force_name (
-  id INTEGER,
-  name VARCHAR(256) NOT NULL
+  id INTEGER DEFAULT 1 UNIQUE,
+  name VARCHAR(256)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
