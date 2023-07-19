4a8e2fdadfe8dfa826d723282d749b781bf7f614
Castro Hamiltone <castromiltone@gmail.com>
2023-07-20 00:39:27 +0300

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,6 @@
 -- script crestes db and user
-DROP DATABASE IF EXISTS hbtn_0d_2;
-CREATE DATABASE hbtn_0d_2;
-use hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@localhost;
+GRANT SELECT ON * . * TO 'user_0d_2'@localhost;
 FLUSH PRIVILEGES;
