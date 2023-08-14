c94f811dc59876bc83fc4ddc818df7b6e2b19d77
Castro Hamiltone <castromiltone@gmail.com>
2023-08-14 21:14:48 +0300

    codestyle fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,8 +3,8 @@ const { argv } = process;
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-console.log(add(a, b))
+console.log(add(a, b));
