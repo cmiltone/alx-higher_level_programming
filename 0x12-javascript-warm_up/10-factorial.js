b8fe9f234e83243c59c621cdeff0ee71e7e42339
Castro Hamiltone <castromiltone@gmail.com>
2023-08-14 21:26:54 +0300

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = process;
+const num = parseInt(argv[2]);
+
+function factorial (num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(num));
