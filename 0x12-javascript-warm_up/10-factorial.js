5e57e277aba176c385c62f5ab5f93d6053802419
Castro Hamiltone <castromiltone@gmail.com>
2023-08-14 21:44:45 +0300

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ const { argv } = process;
 const num = parseInt(argv[2]);
 
 function factorial (num) {
-  if (num === 1) {
+  if (num === 1 || isNaN(num)) {
     return 1;
   }
   return num * factorial(num - 1);
