92fe045a6247299ee3c059d23dc6629ec89b0934
Castro Hamiltone <castromiltone@gmail.com>
2023-08-14 20:01:03 +0300

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 const { argv } = process;
 const args = argv.filter((_, i) => i > 1);
 
-if (args.length) {
-  console.log(args.join(''));
-} else {
+if (args.length <= 0) {
   console.log('No argument');
+} else if (args.length >= 2) {
+  console.log('Arguments found');
+} else {
+  console.log('Argument found');
 }
