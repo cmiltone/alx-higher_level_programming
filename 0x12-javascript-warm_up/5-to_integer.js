e45ffb62b9daba6a946d845880232e4dc0ab86ab
Castro Hamiltone <castromiltone@gmail.com>
2023-08-14 20:47:49 +0300

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = process;
 const num = Math.floor(Number(argv[2]));
-if (!isNaN(num)) {
-  console.log(`My number is: ${num}`);
-} else {
+if (isNaN(num)) {
   console.log('Not a number');
+} else {
+  console.log(`My number is: ${num}`);
 }
