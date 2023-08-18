f95c8b57c4e2459a6c273a9e2218a341a93ffbd2
Castro Hamiltone <castromiltone@gmail.com>
2023-08-18 04:40:19 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,21 @@
 """module lists states from db hbtn_0e_0_usa"""
 
 import MySQLdb
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+
+print ("name: {}, passwod: {}, db: {}".format(username, password, dbname))
+
+try:
+    conn= MySQLdb.connect("127.0.0.1", username, password, dbname)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print ("{}".format(row))
+except Exception as e:
+  print("Something went wrong... ")
+  print(e)
