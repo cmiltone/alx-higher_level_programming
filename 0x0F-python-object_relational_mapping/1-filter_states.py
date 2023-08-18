b2fcfa4609423cebcc21f1dbfed5bcc237112a22
cmiltone <castromiltone@gmail.com>
2023-08-18 15:56:18 +0300

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module lists states from db hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    try:
+        conn = MySQLdb.connect("127.0.0.1", username, password, dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC WHERE UPPER(name) LIKE 'N%'")
+        rows = cur.fetchall()
+        for row in rows:
+            print("{}".format(row))
+    except Exception as e:
+        print("Something went wrong... ")
+        print(e)
