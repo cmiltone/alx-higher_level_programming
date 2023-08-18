206c0f386e2d202c32aea0d740de95673f6d9bb1
cmiltone <castromiltone@gmail.com>
2023-08-18 14:50:58 +0300

    codestyle fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,15 +8,15 @@ username = sys.argv[1]
 password = sys.argv[2]
 dbname = sys.argv[3]
 
-print ("name: {}, passwod: {}, db: {}".format(username, password, dbname))
+
 
 try:
-    conn= MySQLdb.connect("127.0.0.1", username, password, dbname)
+    conn = MySQLdb.connect("127.0.0.1", username, password, dbname)
     cur = conn.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
-        print ("{}".format(row))
+        print("{}".format(row))
 except Exception as e:
-  print("Something went wrong... ")
-  print(e)
+    print("Something went wrong... ")
+    print(e)
