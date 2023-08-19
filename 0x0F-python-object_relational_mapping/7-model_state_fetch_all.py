7d800c19885234e91a04bafd19aa1c5c1585829d
Castro Hamiltone <castromiltone@gmail.com>
2023-08-19 00:09:14 -0700

    7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module uses State model to list states"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pswd = sys.argv[2]
+    dname = sys.argv[3]
+    conn_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(uname, pswd, dname)
+    engn = create_engine(conn_str, pool_pre_ping=True)
+    Base.metadata.create_all(engn)
+    Session = sessionmaker(bind=engn)
+    sessn = Session()
+    for state in sessn.query(State).order_by(State.id).all():
+        print('{}: {}'.format(state.id, state.name))
+
+    sessn.close()
