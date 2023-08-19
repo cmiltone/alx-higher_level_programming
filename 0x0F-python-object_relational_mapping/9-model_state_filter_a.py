f0ca88794565534af12e52f9b0ee86654d9cc5dd
Castro Hamiltone <castromiltone@gmail.com>
2023-08-19 00:58:21 -0700

    9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module uses State model to list states"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pswd = sys.argv[2]
+    dname = sys.argv[3]
+    conn_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(uname, pswd, dname)
+    engn = create_engine(conn_str, pool_pre_ping=True)
+    Base.metadata.create_all(engn)
+    Session = sessionmaker(bind=engn)
+    sessn = Session()
+    search = "%a%"
+    for state in sessn.query(State).filter(State.name.contains('a')):
+        print('{}: {}'.format(state.id, state.name))
+
+    sessn.close()
