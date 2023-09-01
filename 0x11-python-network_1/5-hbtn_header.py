a9ee6ca7437339b4eb107e8111ffe1152564ef77
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 20:24:28 +0300

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,6 +4,7 @@ module akes in a URL, sends a request
 to the URL and displays the value of
 the X-Request-Id variable found in
 the header of the response
+using requests
 """
 
 
@@ -12,5 +13,5 @@ if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     res = requests.get(url)
-    header = res.headers['X-Request-Id']
+    header = res.headers.get('X-Request-Id')
     print(header)
