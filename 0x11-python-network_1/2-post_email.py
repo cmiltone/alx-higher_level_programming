8a159a11de36af1570088bd8cbf24cba2d1c620e
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 19:55:28 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module fetches from
+https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    url = argv[1]
+    email = argv
+    data = parse.urlencode({'email': email}).encode()
+    req = request.Request(url, data=data)
+    with request.urlopen(req) as response:
+        content = response.read()
+        print("{}".format(content.decode("utf-8")))
