b5c9e6185cbfc2b24f5e00e1600ae0a3e23ff9a6
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 19:23:56 +0300

    codestyle fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ https://alx-intranet.hbtn.io/status
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status/') as response:
+    url = 'https://alx-intranet.hbtn.io/status/'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
