8eca78ab9a2c14c8b2f60b61f6379ee6d4c6d456
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 20:00:43 +0300

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     from urllib import request, parse
     from sys import argv
     url = argv[1]
-    email = argv
+    email = argv[2]
     data = parse.urlencode({'email': email}).encode()
     req = request.Request(url, data=data)
     with request.urlopen(req) as response:
