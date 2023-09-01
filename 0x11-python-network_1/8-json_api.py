db76060e1658f161031b9dc83aac497824af731c
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 21:32:30 +0300

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+module  sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = argv[1]
+    data = {'q': letter}
+    req = requests.post(url, data=data)
+    try:
+        json = req.json()
+        if json == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
