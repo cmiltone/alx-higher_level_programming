f451f7fe2a6ab7c97c588f6a91d60178ec1d5860
Castro Hamiltone <castromiltone@gmail.com>
2023-09-01 20:31:10 +0300

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module fetches from
+https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+    req = requests.post(url, json=data)
+    print("{}".format(req.text))
