cb9048135d77ec6dbefea661b8dabd7a15ac630d
Castro Hamiltone <castromiltone@gmail.com>
2023-10-05 01:20:54 +0300

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.ajax(url).done((res) => {
+  for (let i = 0; i < res.results.length; i++) {
+    const el = res.results[i];
+    $('ul#list_movies').append(`<li>${el.title}</li>`);
+  }
+})
